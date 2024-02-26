61e8ec4ce6b962d4481f03b9687fdfee535dca63
Ivan <ivan120536@gmail.com>
2024-02-26 15:25:06 +0400

    refactor: - Little fixes for database path - updated .gitignore - removed greenlet from requirements.txt, since it doesn't needed and actually does not compiles for some reason

--- wsgi.py
+++ wsgi.py
@@ -2,5 +2,5 @@ from main import app
 from data import db_session
 
 if __name__ == "__main__":
-    db_session.global_init("db/mars_explorer.sqlite")
+    db_session.global_init("./db/mars_explorer.sqlite")
     app.run()
