c2b42fb5fd9a610ededd59b69947c2c5f717beab
Ivan <ivan120536@gmail.com>
2024-02-26 19:56:57 +0400

    refactor: Refactored imports in model class `User`

--- data/users.py
+++ data/users.py
@@ -1,7 +1,7 @@
 from datetime import datetime as dt
 
+from sqlalchemy import Column, Integer, String, DateTime
 from flask_login import UserMixin
-import sqlalchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from .db_session import SqlAlchemyBase
@@ -10,17 +10,17 @@ from .db_session import SqlAlchemyBase
 class User(SqlAlchemyBase, UserMixin):
     __tablename__ = 'users'
 
-    id = sqlalchemy.Column(sqlalchemy.Integer,
+    id = Column(Integer,
                            primary_key=True, autoincrement=True)
-    surname = sqlalchemy.Column(sqlalchemy.String)
-    name = sqlalchemy.Column(sqlalchemy.String)
-    age = sqlalchemy.Column(sqlalchemy.Integer)
-    position = sqlalchemy.Column(sqlalchemy.String)
-    speciality = sqlalchemy.Column(sqlalchemy.String)
-    address = sqlalchemy.Column(sqlalchemy.String)
-    email = sqlalchemy.Column(sqlalchemy.String, unique=True)
-    hashed_password = sqlalchemy.Column(sqlalchemy.String)
-    modified_date = sqlalchemy.Column(sqlalchemy.DateTime, default=dt.now)
+    surname = Column(String)
+    name = Column(String)
+    age = Column(Integer)
+    position = Column(String)
+    speciality = Column(String)
+    address = Column(String)
+    email = Column(String, unique=True)
+    hashed_password = Column(String)
+    modified_date = Column(DateTime, default=dt.now)
 
     def __repr__(self):
         return f'<Colonist> {self.id} {self.name} {self.surname}'
