e694fa0cc62bd62a46e505cc458e3de3b05b2e52
Ivan <ivan120536@gmail.com>
2024-02-26 19:53:46 +0400

    refactor(model)!: Refactored model class `Job`
    BREAKING CHANGES: Model got renamed from `Jobs` -> `Job`

--- data/jobs.py
+++ data/jobs.py
@@ -1,22 +1,21 @@
-import sqlalchemy
-from sqlalchemy import orm
+from sqlalchemy import Column, Integer, ForeignKey, String, DateTime, Boolean
+from sqlalchemy.orm import relation
 from datetime import datetime as dt
 
 from .db_session import SqlAlchemyBase
 
 
-class Jobs(SqlAlchemyBase):
-    __tablename__ = 'jobs'
+class Job(SqlAlchemyBase):
+    __tablename__ = "jobs"
 
-    id = sqlalchemy.Column(sqlalchemy.Integer,
-                           primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
 
-    leader_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("users.id"))
-    leader = orm.relation('User')
+    leader_id = Column(Integer, ForeignKey("users.id"))
+    leader = relation("User")
 
-    job = sqlalchemy.Column(sqlalchemy.String)
-    work_size = sqlalchemy.Column(sqlalchemy.Integer)
-    collaborators = sqlalchemy.Column(sqlalchemy.String)
-    start_date = sqlalchemy.Column(sqlalchemy.DateTime, default=dt.now)
-    end_date = sqlalchemy.Column(sqlalchemy.DateTime)
-    is_finished = sqlalchemy.Column(sqlalchemy.Boolean)
+    job = Column(String)
+    work_size = Column(Integer)
+    collaborators = Column(String)
+    start_date = Column(DateTime, default=dt.now)
+    end_date = Column(DateTime)
+    is_finished = Column(Boolean)
