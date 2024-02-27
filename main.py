f1e42fa71abe4eea4f53f1cae27fe5212f243d57
Ivan <ivan120536@gmail.com>
2024-02-27 19:41:12 +0400

    feature(job): Added job creation feature - Added corresponding handler `create_job()` in main.py - Added corresponding template `create_job.html`

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, redirect
 from flask_login import LoginManager, login_user, logout_user
 from flask_wtf import FlaskForm
 
-from wtforms import PasswordField, StringField, IntegerField, EmailField, SubmitField, BooleanField
+from wtforms import PasswordField, StringField, IntegerField, EmailField, SubmitField, BooleanField, DateField
 from wtforms.validators import DataRequired
 
 from data import db_session
@@ -126,6 +126,48 @@ def login():
                                message="Неправильный логин или пароль",
                                **params)
     return render_template('login.html', **params)
+
+
+class JobForm(FlaskForm):
+    job = StringField('Работа', validators=[DataRequired()])
+    leader_id = StringField('Индетификатор лидера', validators=[DataRequired()])
+    work_size = IntegerField('Размер работы (в часах)', validators=[DataRequired()])
+    collaborators = StringField('Участники', validators=[DataRequired()])
+    start_date = DateField('Дата начала')
+    end_date = DateField('Дата конца', validators=[DataRequired()])
+    is_finished = BooleanField('Закончена ли работа?')
+    submit = SubmitField('Создать')
+    
+
+
+@app.route('/create_job', methods=["GET", "POST"])
+def create_job():
+    db_session.global_init("db/mars_explorer.sqlite")
+    form = JobForm()
+    params = {
+        'title': 'Создание записи о работе',
+        'navbar_title': 'Миссия Колонизация Марса',
+        'form': form,
+        'hrefs': return_links()
+    }
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        leader = db_sess.query(User).filter(User.id == form.leader_id.data).first()
+        if leader is None:
+            return render_template('create_job.html', **params, message="Лидер не найден!")
+        job = Job(
+            leader = leader,
+            job = form.job.data,
+            work_size = form.work_size.data,
+            collaborators = form.collaborators.data,
+            start_date = form.start_date.data,
+            end_date = form.end_date.data,
+            is_finished = form.is_finished.data,
+        )
+        db_sess.add(job)
+        db_sess.commit()
+        return redirect('/')
+    return render_template('create_job.html', **params)
 
 
 @app.route('/logout')
